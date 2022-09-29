125418a1c15bbaab6f727701f327140c13f263df
devsuntech <sandeepdhabhai2016@gmail.com>
2022-09-30 01:36:56 +0530

    controller added

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -55,7 +55,7 @@ class BookingController extends Controller
         ];
         $booking->fill($booking_data)->save();
         $booking->bookingDetail()->saveMany($this->prepareBookingDetailArray($validated));
-
+        Mail::to(Auth::user()->email)->send(new EmailBookingUser());
         return response()->json($booking);
     }
 
@@ -118,7 +118,7 @@ class BookingController extends Controller
         ])->id;
         $booking->save();
         $booking->bookingDetail()->saveMany($this->prepareBookingDetailArray($validated));
-        Mail::to(Auth::user()->email)->send(new EmailBookingUser());
+        
         return response()->json($booking);
     }
 
